8805906c50f682d85ae884bf772e5b81a9213206
Sorin B <niros98@gmail.com>
2018-10-14 02:08:10 +0300

    .

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,18 @@
-var player1;
-var player2;
-var ball;
+var Players;
 var myScore;
 
 function startGame() {
-	player1 = new component(1, 20, "#000000", 2, 50);
-	player2 = new component(1, 20, "#000000", 98, 50);
-	ball = 		new component(2, 4,	 "#000000", 50, 50);
 	myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+
+	P1 = new Player(2,  50, 1, 20, "#FF0000", .2, .65);
+	P2 = new Player(98, 50, 1, 20, "#FF0000", .2, .65);
+	B =  new Ball	 (50, 50, 2, 4,  "#000000",  .5);
+
+	Player.push(P1);
+	Player.push(P2);
 }
 
-var myGameArea = {
+var gameArea = {
 	//canvas : document.createElement("canvas"),
 	canvas : document.getElementById("gameArea"),
 	start : function() {
@@ -23,14 +25,10 @@ var myGameArea = {
 			this.canvas.height = window.innerHeight;
 		}
 		this.context = this.canvas.getContext("2d");
+
 		//document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		window.clearInterval(this.interval);
-		this.interval = setInterval(updateGameArea, 5);
-
-		player1.y = player2.y = 50;
-
-		//player1.x = 0;
-		//player1.y = (window.innerHeight - player1.height) / 2;
+		this.interval = setInterval(updateGameArea, 2);
 	},
 	clear : function() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -52,26 +50,26 @@ function component(width, height, color, x, y, type) {
 	this.velocityY = 0;
 	this.friction = 0.65;
 
-	this.x = x;
-	this.y = y;
+	this.X = x;
+	this.Y = y;
 
 	this.update = function() {
-		ctx = myGameArea.context;
+		ctx = gameArea.context;
 		if (this.type == "text") {
 			ctx.font = this.width + " " + this.height;
 			ctx.fillStyle = color;
 			ctx.fillText(
 				this.text,
-				this.x * myGameArea.screenRatio()[0],
-				this.y * myGameArea.screenRatio()[1]);
+				this.X * gameArea.screenRatio()[0],
+				this.Y * gameArea.screenRatio()[1]);
 		} else {
 			ctx.fillStyle = color;
-			//ctx.fillRect(this.x, this.y, this.width, this.height);
+			//ctx.fillRect(this.X, this.Y, this.width, this.height);
 			ctx.fillRect(
-				(this.x - this.width / 2)		* myGameArea.screenRatio()[0],
-				(this.y - this.height / 2)	* myGameArea.screenRatio()[1],
-				this.width  								* myGameArea.screenRatio()[0],
-				this.height 								* myGameArea.screenRatio()[1]
+				(this.X - this.width / 2)		* gameArea.screenRatio()[0],
+				(this.Y - this.height / 2)	* gameArea.screenRatio()[1],
+				this.width  								* gameArea.screenRatio()[0],
+				this.height 								* gameArea.screenRatio()[1]
 			);
 		}
 
@@ -93,8 +91,8 @@ function component(width, height, color, x, y, type) {
 		this.velocityX += this.speed;
 	}
 	this.newPos = function() {
-		this.x += this.velocityX;
-		this.y += this.velocityY;
+		this.X += this.velocityX;
+		this.Y += this.velocityY;
 
 		this.velocityY *= this.friction;
 		this.velocityX *= this.friction;
@@ -112,27 +110,27 @@ function component(width, height, color, x, y, type) {
 	}
 	this.hitBottom = function() {
 		var b = 100 - this.height / 2;
-		if (this.y > b) {
-			this.y = b;
+		if (this.Y > b) {
+			this.Y = b;
 			this.velocityY = 0;
 		}
 	}
 	this.hitTop = function() {
 		var t = this.height / 2;
-		if (this.y < t) {
-			this.y = t;
+		if (this.Y < t) {
+			this.Y = t;
 			this.velocityY = 0;
 		}
 	}
 	this.crashWith = function(otherobj) {
-		var myleft = this.x;
-		var myright = this.x + (this.width);
-		var mytop = this.y;
-		var mybottom = this.y + (this.height);
-		var otherleft = otherobj.x;
-		var otherright = otherobj.x + (otherobj.width);
-		var othertop = otherobj.y;
-		var otherbottom = otherobj.y + (otherobj.height);
+		var myleft = this.X;
+		var myright = this.X + (this.width);
+		var mytop = this.Y;
+		var mybottom = this.Y + (this.height);
+		var otherleft = otherobj.X;
+		var otherright = otherobj.X + (otherobj.width);
+		var othertop = otherobj.Y;
+		var otherbottom = otherobj.Y + (otherobj.height);
 		var crash = true;
 		if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
 			crash = false;
@@ -141,67 +139,218 @@ function component(width, height, color, x, y, type) {
 	}
 }
 
+class Component {
+	constructor(x, y, width, height, color) {
+		this.X = x;
+		this.Y = y;
+		this.width = width;
+		this.height = height;
+		this.speed = 0;
+		//this.maxSpeed = 2;
+		this.velocityX = 0;
+		this.velocityY = 0;
+		this.friction = 1;
+	}
+	draw() {}
+	collisions() {}
+	updatePosition() {
+		this.X += this.velocityX;
+		this.Y += this.velocityY;
+
+		this.velocityX *= this.friction;
+		this.velocityY *= this.friction;
+
+		this.velocityX = Math.round(this.velocityX * 1000) / 1000;
+		this.velocityY = Math.round(this.velocityY * 1000) / 1000;
+		if(Math.abs(this.velocityX) == 0.001) this.velocityX = 0;
+		if(Math.abs(this.velocityY) == 0.001) this.velocityY = 0;
+
+		this.collisions();
+	}
+	update() {}
+	moveUp(val) {
+		this.velocityY += this.speed;
+	}
+	moveDown(val) {
+		this.velocityY -= this.speed;
+	}
+	moveLeft(val) {
+		this.velocityX -= this.speed;
+	}
+	moveRight(val) {
+		this.velocityX += this.speed;
+	}
+}
+
+class Player extends Component {
+	constructor(x, y, width, height, color, speed, friction) {
+		super(x, y, width, height, color);
+		this.speed = speed;
+		this.friction = friction;
+	}
+	draw() {
+		ctx = gameArea.context;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(
+			(this.X - this.width / 2)		* gameArea.screenRatio()[0],
+			(this.Y - this.height / 2)	* gameArea.screenRatio()[1],
+			this.width  								* gameArea.screenRatio()[0],
+			this.height 								* gameArea.screenRatio()[1]
+		);
+	}
+	update() {
+		super.update();
+		super.updatePosition();
+		this.collisions();
+	}
+	collisions() {
+		var t = this.height / 2;
+		var b = 100 - this.height / 2;
+		if(this.Y < t) {
+			this.Y = t;
+			this.velocityY = 0;
+		}
+		if(this.Y > b) {
+			this.Y = b;
+			this.velocityY = 0;
+		}
+	}
+};
+
+class Ball extends Component {
+	constructor(x, y, width, height, color, velocity) {
+		super(x, y, width, height, color);
+
+		//this.velocityX = velocity * (Math.random() < 0.5 ? -1 : 1);
+		//this.velocityY = 0;
+		this.velocityX = velocity * (Math.random() * 2 - 1);
+		this.velocityY = velocity * (Math.random() * 2 - 1);
+
+	}
+	draw() {
+		ctx = gameArea.context;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(
+			(this.X - this.width / 2)		* gameArea.screenRatio()[0],
+			(this.Y - this.height / 2)	* gameArea.screenRatio()[1],
+			this.width  								* gameArea.screenRatio()[0],
+			this.height 								* gameArea.screenRatio()[1]
+		);
+	}
+	update() {
+		super.update();
+		super.updatePosition();
+		this.collisions();
+	}
+	collisions() {
+		var t = this.height / 2;
+		var b = 100 - this.height / 2;
+		var l = this.width / 2;
+		var r = 100 - this.width / 2;
+
+		if(this.X < l) {
+			this.velocityX *=  -1;
+			this.X = l;
+		}
+		if(this.X > r) {
+			this.velocityX *=  -1;
+			this.X = r;
+		}
+		if(this.Y < t) {
+			this.velocityY *= -1;
+			this.Y = t;
+		}
+		if(this.Y > b) {
+			this.velocityY *= -1;
+			this.Y = b;
+		}
+	}
+}
+
 function updateGameInfo() {
 	var p1Info = document.getElementById("player1Info");
 	p1Info.innerHTML = "\
 	<ul> \
-		<li><b>Width: </b>" 		+ player1.width + "</li> \
-		<li><b>Height: </b>" 		+ player1.height + "</li> \
-		<li><b>Velocity: </b>" 	+ player1.velocityY + "</li> \
-		<li><b>X: </b>" 				+ player1.x + "</li> \
-		<li><b>Y: </b>" 				+ player1.y + "</li> \
-		<li><b>Speed: </b>" 		+ player1.speed + "</li> \
-		<li><b>Friction: </b>" 	+ player1.friction + "</li> \
+		<li><b>Width: </b>" 		+ P1.width + "</li> \
+		<li><b>Height: </b>" 		+ P1.height + "</li> \
+		<li><b>Velocity: </b>" 	+ P1.velocityY + "</li> \
+		<li><b>X: </b>" 				+ P1.X + "</li> \
+		<li><b>Y: </b>" 				+ P1.Y + "</li> \
+		<li><b>Speed: </b>" 		+ P1.speed + "</li> \
+		<li><b>Friction: </b>" 	+ P1.friction + "</li> \
 	</ul> \
-	";var p1Info = document.getElementById("player1Info");
-
+	";
+	var bInfo = document.getElementById("ballInfo");
+	bInfo.innerHTML = "\
+	<ul> \
+		<li><b>Width: </b>" 		+ B.width + "</li> \
+		<li><b>Height: </b>" 		+ B.height + "</li> \
+		<li><b>VelocityX: </b>" + B.velocityX + "</li> \
+		<li><b>VelocityY: </b>" + B.velocityY + "</li> \
+		<li><b>X: </b>" 				+ B.X + "</li> \
+		<li><b>Y: </b>" 				+ B.Y + "</li> \
+		<li><b>Speed: </b>" 		+ B.speed + "</li> \
+		<li><b>Friction: </b>" 	+ B.friction + "</li> \
+	</ul> \
+	";
 	var p2Info = document.getElementById("player2Info");
 	p2Info.innerHTML = "\
 	<ul> \
-		<li><b>Width: </b>" 		+ player2.width + "</li> \
-		<li><b>Height: </b>" 		+ player2.height + "</li> \
-		<li><b>Velocity: </b>" 	+ player2.velocityY + "</li> \
-		<li><b>X: </b>" 				+ player2.x + "</li> \
-		<li><b>Y: </b>" 				+ player2.y + "</li> \
-		<li><b>Speed: </b>" 		+ player2.speed + "</li> \
-		<li><b>Friction: </b>" 	+ player2.friction + "</li> \
+		<li><b>Width: </b>" 		+ P2.width + "</li> \
+		<li><b>Height: </b>" 		+ P2.height + "</li> \
+		<li><b>Velocity: </b>" 	+ P2.velocityY + "</li> \
+		<li><b>X: </b>" 				+ P2.X + "</li> \
+		<li><b>Y: </b>" 				+ P2.Y + "</li> \
+		<li><b>Speed: </b>" 		+ P2.speed + "</li> \
+		<li><b>Friction: </b>" 	+ P2.friction + "</li> \
 	</ul> \
 	";
 }
 
+function gameCollisions() {
+	for(i = 0; i < Players.length(); i++) {}
+}
+
 function updateGameArea() {
 	updateGameInfo();
-	myGameArea.clear();
+	gameArea.clear();
+	gameArea.context.beginPath();
+	gameArea.context.moveTo(gameArea.canvas.width / 2, 							 			0);
+	gameArea.context.lineTo(gameArea.canvas.width / 2, gameArea.canvas.height);
+	gameArea.context.strokeStyle = "#E2E2E2";
+	gameArea.context.lineWidth = 1.5;
+	gameArea.context.stroke();
 	/*
 	myScore.text =
-		(player1.x			* myGameArea.screenRatio()[0]) + "\n" +
-		(player1.y			* myGameArea.screenRatio()[1]) + "\n" +
-		(player1.width  * myGameArea.screenRatio()[0]) + "\n" +
-		(player1.height * myGameArea.screenRatio()[1]) + "\n" +
+		(player1.X			* gameArea.screenRatio()[0]) + "\n" +
+		(player1.Y			* gameArea.screenRatio()[1]) + "\n" +
+		(player1.width  * gameArea.screenRatio()[0]) + "\n" +
+		(player1.height * gameArea.screenRatio()[1]) + "\n" +
 		player1.velocityY;
 	*/
 	//myScore.text = player1.velocityY;
 	myScore.text = "";
 	myScore.update();
-	player1.newPos();
-	player1.update();
-	player2.newPos();
-	player2.update();
-	ball.newPos();
-	ball.update();
+
+	P1.update();
+	P1.draw();
+	P2.update();
+	P2.draw();
+	B.update();
+	B.draw();
 
 
 	if (keyState[38]) {
-		player1.moveDown();
+		P1.moveDown();
 	}
 	if (keyState[40]) {
-		player1.moveUp();
+		P1.moveUp();
 	}
 	if (keyState[87]) {
-		player2.moveDown();
+		P2.moveDown();
 	}
 	if (keyState[83]) {
-		player2.moveUp();
+		P2.moveUp();
 	}
 }
 
@@ -217,12 +366,12 @@ window.addEventListener('keyup', function(e) {
 
 window.addEventListener('resize', (event) => {
 	if(window.innerHeight > window.innerWidth / 2) {
-		myGameArea.canvas.width = window.innerWidth;
-		myGameArea.canvas.height = window.innerWidth / 2;
+		gameArea.canvas.width = window.innerWidth;
+		gameArea.canvas.height = window.innerWidth / 2;
 	}
 	else {
-		myGameArea.canvas.width = window.innerHeight * 2;
-		myGameArea.canvas.height = window.innerHeight;
+		gameArea.canvas.width = window.innerHeight * 2;
+		gameArea.canvas.height = window.innerHeight;
 	}
 	console.log("muie");
 });
@@ -230,7 +379,7 @@ window.addEventListener('resize', (event) => {
 window.addEventListener('keyup', (event) => {
 });
 window.addEventListener('keydown', (event) => {
-	if(event.keyCode == 13) myGameArea.start(), event.preventDefault();
+	if(event.keyCode == 13) gameArea.start(), event.preventDefault();
 	if(event.keyCode == 38) player1.moveDown(), event.preventDefault();
 	if(event.keyCode == 40) player1.moveUp()	, event.preventDefault();
 });
